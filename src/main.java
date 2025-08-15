dbaebf5be3465fceb7cf09d07238d7e99eb560cd
kbrk <xkyburkax@mail.ru>
2025-08-15 23:52:45 +0300

    с ума нах сошёл

--- src/main.java
+++ src/main.java
@@ -1,13 +1,28 @@
 import java.util.Random;
 class Main {
+    private static double calcul(long wi, double xj)
+    {
+        if (wi==14)
+        {
+            return (Math.cbrt(Math.atan((xj + 2) / 8)) - (1.0 / 3.0)) / (Math.pow(Math.E, Math.tan(xj)));
+        }
+        else if (wi==6 || wi==12 || wi==16)
+        {
+            return Math.pow(((1.0/3.0)/Math.tan(Math.pow(2*xj,xj))), 2.0);
+        }
+        else
+        {
+            return Math.atan(1/Math.pow(Math.E, Math.abs(Math.tan(Math.pow(Math.sin(xj), (4*Math.cos(xj)))))));
+        }
+    }
     public static void main(String[] args) {
         long w[];
         w = new long[(16-4)/2+1];
         int c=0;
         for (int i = 16; i >= 4; i-=2)
         {
-           w[c]=i;
-           c++;
+            w[c]=i;
+            c++;
         }
         double x[];
         x = new double[10];
@@ -23,18 +38,7 @@ class Main {
         {
             for (int j = 0; j < b[i].length; j++)
             {
-                if (w[i]==14)
-                {
-                    b[i][j] = (Math.cbrt(Math.atan((x[j] + 2) / 8)) - (1.0 / 3.0)) / (Math.pow(Math.E, Math.tan(x[j])));
-                }
-                else if (w[i]==6 || w[i]==12 || w[i]==16)
-                {
-                    b[i][j]=Math.pow(((1.0/3.0)/Math.tan(Math.pow(2*x[j],x[j]))), 2.0);
-                }
-                else
-                {
-                    b[i][j]=Math.atan(1/Math.pow(Math.E, Math.abs(Math.tan(Math.pow(Math.sin(x[j]), (4*Math.cos(x[j])))))));
-                }
+                b[i][j] = calcul(w[i], x[j]);
                 System.out.printf("%.4f\t", b[i][j]);
             }
             System.out.println("\n");
