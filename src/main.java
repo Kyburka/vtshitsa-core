453f9b0219eb3d100fe6d354d43cba10a835d74f
A <a@a.com>
2025-08-16 01:13:49 +0300

    even and random numbers

--- src/main.java
+++ src/main.java
@@ -1,4 +1,20 @@
+import java.util.*;
+
+
 class Main {
     public static void main(String[] args) {
+        long[] even = {4, 6, 8, 10, 12, 14, 16};
+
+        System.out.println("even: " + Arrays.toString(even));
+
+        double[] random = new double[10];
+
+        int min = -2;
+        int max = 6;
+
+        for (int i = 0; i < random.length; i++)
+            random[i] = Math.random() * (max - min) + min;
+
+        System.out.println("random: " + Arrays.toString(random));
     }
 }
