8f1a5404269ae355b0805e2e55d2ca7c6b2b1ff8
kbrk <xkyburkax@mail.ru>
2025-08-17 16:54:42 +0300

    123

--- src/main.java
+++ src/main.java
@@ -1,9 +1,10 @@
 import java.util.Random;
+
 class Main {
     public static void main(String[] args) {
         long w[] = new long[7];
         int c = 0;
-        for (int i = 16; i >= 4; i-=2) {
+        for (int i = 16; i >= 4; i -= 2) {
             w[c] = i;
             c++;
         }
@@ -20,22 +21,20 @@ class Main {
         }
         printMatrix(b);
     }
+
     private static double calculateMatrix(long wi, double xj) {
         if (wi == 14) {
             return (Math.cbrt(Math.atan((xj + 2) / 8)) - (1.0 / 3.0)) / (Math.pow(Math.E, Math.tan(xj)));
-        }
-        else if (wi == 6 || wi == 12 || wi == 16) {
+        } else if (wi == 6 || wi == 12 || wi == 16) {
             return Math.pow(((1.0 / 3.0) / Math.tan(Math.pow(2 * xj, xj))), 2.0);
-        }
-        else {
+        } else {
             return Math.atan(1 / Math.pow(Math.E, Math.abs(Math.tan(Math.pow(Math.sin(xj), (4 * Math.cos(xj)))))));
         }
     }
+
     private static void printMatrix(double[][] mtx) {
-        for (int i = 0; i < mtx.length; i++)
-        {
-            for (int j = 0; j < mtx[i].length; j++)
-            {
+        for (int i = 0; i < mtx.length; i++) {
+            for (int j = 0; j < mtx[i].length; j++) {
                 System.out.printf("%10.4f", mtx[i][j]);
             }
             System.out.println();
